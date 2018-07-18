8d1bcc8c4dd3d96d53f5bb875975c8ef3f3e7b13
Stian Didriksen <stipsan@gmail.com>
2018-07-18 20:25:58 +0200

    fix rootDir behavior

--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -1,7 +1,9 @@
 import scrollIntoView from '../src'
 
+jest.mock('scroll-into-view-if-needed', () => () => [])
+
 test('options is optional', async () => {
   expect.assertions(1)
   const result = await scrollIntoView(document.body)
-  expect(result).toBe([])
+  expect(result).toEqual([])
 })
