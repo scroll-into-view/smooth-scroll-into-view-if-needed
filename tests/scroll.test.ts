826ce19f78c925e6a27dbea1a02e7ced3a71697b
Stian Didriksen <stipsan@gmail.com>
2018-07-18 22:13:27 +0200

    add test case for no-op

--- tests/scroll.test.ts
+++ tests/scroll.test.ts
@@ -1,16 +1,33 @@
-import scrollIntoView from '../src'
+beforeEach(() => {
+  jest.resetModules()
+})
 
-jest.mock(
-  'scroll-into-view-if-needed',
-  () => (_target: Element, options: any) =>
-    options.behavior([
-      { el: { scrollTop: 0, scrollLeft: 0 }, top: 60, left: 60 },
-    ])
-)
-
-test('options is optional', () => {
+test('resolves after scroll is completed', () => {
+  jest.doMock(
+    'scroll-into-view-if-needed',
+    () => (_target: Element, options: any) =>
+      options.behavior([
+        { el: { scrollTop: 0, scrollLeft: 0 }, top: 60, left: 60 },
+      ])
+  )
+  const scrollIntoView = require('../src').default
   expect.assertions(1)
   return expect(scrollIntoView(document.body)).resolves.toEqual([
     { el: { scrollTop: 60, scrollLeft: 60 }, top: [0, 60], left: [0, 60] },
   ])
+})
+
+test('resolves right away if there is nothign to scroll', () => {
+  jest.doMock(
+    'scroll-into-view-if-needed',
+    () => (_target: Element, options: any) =>
+      options.behavior([
+        { el: { scrollTop: 0, scrollLeft: 0 }, top: 0, left: 0 },
+      ])
+  )
+  const scrollIntoView = require('../src').default
+  expect.assertions(1)
+  return expect(
+    scrollIntoView(document.body, { duration: Number.MAX_VALUE })
+  ).resolves.toEqual([])
 })
