6628b5dfb2801f4da363790e7e47bb8b8444575e
Stian Didriksen <stipsan@gmail.com>
2018-07-18 21:21:53 +0200

    expand test cases

--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -1,11 +1,30 @@
 import scrollIntoView from '../src'
 
+// This mock supports two scenarios:
+// 1. Default smooth scrolling, return empty array to ensure no scrolling is attempted.
+// 2. Falling back to scroll-into-view-if-needed, simulate an array over scroll actions
+// This enables tests to know which of the two cases is in effect, and verify if it's correct.
 jest.mock(
   'scroll-into-view-if-needed',
-  () => (_target: Element, options: any) => options.behavior([])
+  () => (_target: Element, options: any) =>
+    options.behavior ? options.behavior([]) : [{ el: {}, top: 0, left: 0 }]
 )
 
 test('options is optional', () => {
   expect.assertions(1)
   return expect(scrollIntoView(document.body)).resolves.toEqual([])
+})
+
+test('behavior option is optional', () => {
+  expect.assertions(1)
+  return expect(
+    scrollIntoView(document.body, { scrollMode: 'if-needed' })
+  ).resolves.toEqual([])
+})
+
+test('behavior option set to smooth works correctly', () => {
+  expect.assertions(1)
+  return expect(
+    scrollIntoView(document.body, { behavior: 'smooth' })
+  ).resolves.toEqual([])
 })
