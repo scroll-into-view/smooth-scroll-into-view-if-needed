eaf2582c7e4c36af63e17165fa791f2b2c575fe5
Cody Olsen <stipsan@gmail.com>
2023-03-09 21:03:40 +0100

    feat: add ESM
    BREAKING CHANGE: Updates `scroll-into-view-if-needed` to `v3`

--- package.config.ts
+++ package.config.ts
@@ -2,4 +2,11 @@ import { defineConfig } from '@sanity/pkg-utils'
 
 export default defineConfig({
   minify: true,
+
+  extract: {
+    rules: {
+      'ae-missing-release-tag': 'off',
+      'ae-forgotten-export': 'off',
+    },
+  },
 })
--- tests/scroll.test.ts
+++ tests/scroll.test.ts
@@ -10,7 +10,7 @@ test('resolves after scroll is completed', () => {
         { el: { scrollTop: 0, scrollLeft: 0 }, top: 60, left: 60 },
       ])
   )
-  const scrollIntoView = require('../src').default
+  const scrollIntoView = require('../src')
   expect.assertions(1)
   return expect(scrollIntoView(document.body)).resolves.toEqual([
     { el: { scrollTop: 60, scrollLeft: 60 }, top: [0, 60], left: [0, 60] },
@@ -25,7 +25,7 @@ test('resolves right away if there is nothign to scroll', () => {
         { el: { scrollTop: 0, scrollLeft: 0 }, top: 0, left: 0 },
       ])
   )
-  const scrollIntoView = require('../src').default
+  const scrollIntoView = require('../src')
   expect.assertions(1)
   return expect(
     scrollIntoView(document.body, { duration: Number.MAX_VALUE })
